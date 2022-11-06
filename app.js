6c7a7affb222a3afbd49c4864984e999ce437a1e
QuentinVi <quentin.vi0510@gmail.com>
2022-11-06 21:01:35 +0100

    express02finish

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -13,7 +15,10 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users/:id", movieHandlers.getUserById);
+
 
 app.listen(port, (err) => {
   if (err) {
